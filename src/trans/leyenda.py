aa7e1938b36610b50d644e81ea29f671f7da9313
josesmooth <josesmooth@gmail.com>
2021-03-13 12:15:49 -0400

    agregada la clase Leyenda

---
+++ src/trans/leyenda.py
@@ -0,0 +1,18 @@
+'''
+Created on 13 mar. 2021
+
+@author: jose-lopez
+'''
+
+
+class Leyenda:
+    '''
+    Esta clase modela un objeto que gestiona las \
+    etiquets que describen las variables y sus parametros
+
+    '''
+
+    def __init__(self, params):
+        '''
+        Constructor
+        '''
